7baf0b8eb6e296bcc1f43027cc96b42094d3fee8
Unusual92 <104244417+Unusual92@users.noreply.github.com>
2023-10-05 19:01:20 +0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Shape cube = new Cube(5);
         System.out.println(cube.getVolume());
 
-        Shape pyramid = new Pyramid(6, 4);
+        Shape pyramid = Pyramid.builder().baseLength(6).height(4).build();
         System.out.println(pyramid.getVolume());
     }
 }
